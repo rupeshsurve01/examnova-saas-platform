b7dfbeff9b52f41f725f54416cc9521a89c55e81
Rupesh <rupeshsurve302@gmail.com>
2026-02-27 22:40:25 +0530

    Connected MongoDB Atlas to backend

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv"); //This loads all variables from .env file into: process.env
 const cors = require("cors");
+const connectDB = require("./config/db")
 
 dotenv.config();
+
+connectDB()
 
 const app = express();
 
@@ -15,6 +18,7 @@ app.get("/", (req, res) => {
   res.send("ExamNova API is running...");
 });
 
+console.log("MONGO_URI:", process.env.MONGO_URI);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
