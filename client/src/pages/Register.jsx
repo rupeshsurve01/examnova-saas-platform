a75ede1185bf7c2cfc29f7d166b8d3fbfe922a0c
Rupesh <rupeshsurve302@gmail.com>
2026-03-09 17:50:36 +0530

    feat: Implemented Registration Page with backend API Integration

---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import API from "../services/api";
+import { useNavigate } from "react-router-dom";
+
+function Register() {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+    role: "student"
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await API.post("/auth/register", form);
+      alert("Registration successful");
+      navigate("/");
+    } catch (error) {
+      alert(error.response?.data?.message || "Error");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          name="name"
+          placeholder="Name"
+          onChange={handleChange}
+        />
+
+        <input
+          name="email"
+          placeholder="Email"
+          onChange={handleChange}
+        />
+
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          onChange={handleChange}
+        />
+
+        <select name="role" onChange={handleChange}>
+          <option value="student">Student</option>
+          <option value="teacher">Teacher</option>
+        </select>
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
